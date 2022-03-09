d59da9bd1d7e7e5ae72784392c16fc69872ca71a
Шаталюк Евгения Олеговна <EOShatalyuk@sberbank.ru>
2022-03-09 17:46:45 +0300

    2

---
+++ includes/functions.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   functions.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcharlet <lcharlet@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/09 17:55:14 by bcaffere          #+#    #+#             */
+/*   Updated: 2022/02/15 21:44:39 by lcharlet         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FUNCTIONS_H
+# define FUNCTIONS_H
+
+# include <stdio.h>
+# include <unistd.h> //getcwd
+# include <stdlib.h>
+# include <fcntl.h>
+# include <dirent.h>
+
+/* to readline */
+# include "../readline/readline.h"
+# include "../readline/history.h"
+
+/* to signal */
+# include <signal.h>
+
+/* to stat */
+# include <sys/stat.h> // stat
+
+size_t	ft_strlen(const char *str);
+char	**ft_split(char const *s, char c);
+char	*ft_strdup(const char *src);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	ft_putstr_fd(char *s, int fd);
+
+#endif
---
+++ includes/minishell.h
@@ -0,0 +1,121 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcharlet <lcharlet@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/29 17:07:48 by bcaffere          #+#    #+#             */
+/*   Updated: 2022/02/25 20:41:52 by lcharlet         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "functions.h"
+
+# define PWD_MAX 1024
+# define MODE_FOR_FILE 0640
+
+/* for parsing */
+# define CORRECT 0
+# define NOT_CORRECT 1
+# define EXIST 2
+# define NOT_EXIST 3
+
+typedef struct s_env
+{
+	char			*arg;
+	char			*value;
+	struct s_env	*next;
+	int				vis;
+}	t_env;
+
+typedef struct s_cmd
+{
+	int	fd_in;
+	int	fd_out;
+
+	int	old_fd_in;
+	int	old_fd_out;
+
+	int	is_pipe;
+	int	count_pipe;
+
+	int	is_redirect;
+	int	count_redirect;
+
+	int	is_right;
+	int	count_right;
+
+	int	is_dub_right;
+	int	count_dub_right;
+
+	int	is_left;
+	int	count_left;
+
+	int	is_dub_left;
+	int	count_dub_left;
+
+	int	is_cmd;
+	int	is_status;
+	char **name_cmd;
+}	t_cmd;
+
+typedef struct s_bin
+{
+	int			pid; // ид процесса
+	char		*path;
+	struct stat	*file_stat;
+	char		**argv;
+	char		*cur_dir;
+	char		**env_for_ex;
+	int			*status;
+}	t_bin;
+
+typedef struct s_data
+{
+	char	*line;
+	t_env	*sh_env;
+	char	**args;
+	int		status;
+	t_cmd	*cmd;
+	// t_bin	*bin;
+}	t_data;
+
+
+/* for execute not my programm */
+char 	**convert_sh_env(t_env **sh_env);
+
+/* to work with envp*/
+t_env	*new_env_elem(char *str, int v);
+t_env	*last_elem(t_env *elem);
+void	add_elem_back(t_env **data, t_env *new);
+char	*get_arg(char *str);
+char	*get_value(char *str);
+void	copy_env(t_env **data, char **envp);
+void	env_print(t_env *data);
+
+/* cmd */
+void	cmd_export(char *str, t_env **envp);
+void	cmd_unset(char *str, t_env **sh_env);
+void	cmd_pwd(void);
+void	cmd_cd(char *str);
+void	cmd_echo(char *flag, char *str);
+
+/* execute my programm */
+void	exec_bin(char *str, t_env **sh_env);
+
+/* redirects */
+void    redirect(char *str, char *file, t_data *data);
+int    fd_for_redirect(char *str, char *file, t_data *data);
+
+/* parsing */
+void	common_parsing(t_data *data);
+
+/* my_shell */
+void my_programm(char **name_cmd, t_data *data);
+
+
+#endif
